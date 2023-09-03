cfcbaca09c5c01e846893f8449627f0c4289e997
I02chahboun <ismail02.dev@gmail.com>
2023-09-04 00:34:39 +0200

    add flutter binding & firebase initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:messageme_app/constants/colors.dart';
 import 'package:messageme_app/screens/registration_screen.dart';
 import 'package:messageme_app/screens/signin_screen.dart';
 import 'screens/welcome_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
