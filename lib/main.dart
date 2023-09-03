99d7d640d041f53baa1f846b2c10f6ce4f069fa6
I02chahboun <ismail02.dev@gmail.com>
2023-09-03 17:26:28 +0200

    add  routes & disable debug

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:messageme_app/screens/registration_screen.dart';
+import 'package:messageme_app/screens/signin_screen.dart';
 import 'screens/welcome_screen.dart';
 
 void main() {
@@ -9,11 +11,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MessageMe app',
+      debugShowCheckedModeBanner: false,
+      title: 'Message App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.nameRoute,
+      routes: {
+        WelcomeScreen.nameRoute: (context) => WelcomeScreen(),
+        SignInScreen.nameRoute: (context) => SignInScreen(),
+        RegistrationScreen.nameRoute: (context) => RegistrationScreen(),
+      },
     );
   }
 }
